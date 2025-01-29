2067ec6b65cb87f86ed10776d4cb987e68917ba0
ABHISHEK RANJANAGOUDA G <101989040+abhiranjan2123@users.noreply.github.com>
2025-01-29 13:34:42 +0530

    aded

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -4,7 +4,7 @@ const AvatarPhoto = () => {
   return (
     <div className="flex justify-center mt-30">
    <Avatar className="w-40 h-40 ring-slate-100 ring">
-      <AvatarImage src="https://ik.imagekit.io/5lnqg1qxwh/Myimage.JPG?updatedAt=1738137235111" />
+      <AvatarImage src="https://ik.imagekit.io/5lnqg1qxwh/Myimage.JPG?updatedAt=1738137839270" />
       <AvatarFallback>ARG</AvatarFallback>
     </Avatar>
     </div>
